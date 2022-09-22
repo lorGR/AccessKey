ced1872196fb7bfcbedea03126d87dd204907379
lioGR <liorgrishinnew@gmail.com>
2022-09-22 15:38:39 +0300

    Added Events

--- AccessKey-v1/accesskey.v1/src/features/event/Event.tsx
+++ AccessKey-v1/accesskey.v1/src/features/event/Event.tsx
@@ -1,11 +1,34 @@
-// interface EventProps {
-//     icon: React.FC,
-//     worker: 
-// }
+import { ReactComponent as DoorStatusTrueIcon } from "./assets/svgs/doorStatusTrueIcon.svg";
+import { ReactComponent as DoorStatusFalseIcon } from "./assets/svgs/doorStatusFalseIcon.svg";
 
-const Event = () => {
+interface EventProps {
+  status: boolean,
+  worker: string,
+  event: boolean,
+  door: string,
+  time: string
+}
+
+const Event: React.FC<EventProps> = ({ status, worker, event, door, time }) => {
   return (
-    <div>Event</div>
+    <div className="event">
+      {status === true &&
+        <p>
+          <DoorStatusTrueIcon />
+          {worker}
+          Unlocked the door 
+          {door} 
+          at {time}
+        </p>
+      }
+      {status === false &&
+        <p>
+          <DoorStatusFalseIcon />
+          {door}
+          failed to open at {time}
+        </p>
+      }
+    </div>
   )
 }
 
