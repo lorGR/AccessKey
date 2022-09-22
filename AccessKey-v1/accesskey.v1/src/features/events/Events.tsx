4779eb4a91e827488593549a0b768395196c95e6
lioGR <liorgrishinnew@gmail.com>
2022-09-22 14:36:21 +0300

    Added Events and Event Component

--- AccessKey-v1/accesskey.v1/src/features/events/Events.tsx
+++ AccessKey-v1/accesskey.v1/src/features/events/Events.tsx
@@ -1,5 +1,59 @@
 import EventsButton from "../eventsButton/EventsButton";
 
+import Event from "../event/Event";
+
+const companyEvents = [
+    {
+        status: true,
+        worker: "Solomon Clay",
+        event: true,
+        door: "2H",
+        time: "11:25"
+    },
+    {
+        status: false,
+        worker: "worker",
+        event: false,
+        door: "22C",
+        time: "11:25"
+    },
+    {
+        status: true,
+        worker: "Asha Olson",
+        event: true,
+        door: "2F",
+        time: "11:13"
+    },
+    {
+        status: true,
+        worker: "Lewys Gill",
+        event: true,
+        door: "2F",
+        time: "11:02"
+    },
+    {
+        status: true,
+        worker: "Oskar Cross",
+        event: true,
+        door: "20P",
+        time: "10:46"
+    },
+    {
+        status: true,
+        worker: "Gili Menahem",
+        event: true,
+        door: "27A",
+        time: "10:37"
+    },
+    {
+        status: true,
+        worker: "Lior Grishin",
+        event: true,
+        door: "1D",
+        time: "10:21"
+    }
+]
+
 const Events = () => {
   return (
     <div className="events">
@@ -9,6 +63,13 @@ const Events = () => {
                 <EventsButton btnName="All"/>
                 <EventsButton btnName="Alerts"/>
             </div>
+        </div>
+        <div className="events__events">
+            { companyEvents.map(event => {
+                return (
+                    <Event />
+                )
+            })}
         </div>
     </div>
   );
