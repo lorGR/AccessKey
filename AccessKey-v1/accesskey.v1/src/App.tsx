58d985fe290cf286ac7fc2ca3e37de686d9e1617
lioGR <liorgrishinnew@gmail.com>
2022-09-22 14:59:04 +0300

    WIP

--- AccessKey-v1/accesskey.v1/src/App.tsx
+++ AccessKey-v1/accesskey.v1/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import CompanyMenu from './features/companyMenu/CompanyMenu';
 import Events from './features/events/Events';
+import { InfoTable } from './features/infoTable/InfoTable';
 import Navbar from './features/navbar/Navbar';
 
 function App() {
@@ -14,7 +15,9 @@ function App() {
         <div className="compeny-menu-grid">
           <CompanyMenu locations={11} doors={20} workers={80} />
         </div>
-        <div className="info-table-grid"></div>
+        <div className="info-table-grid">
+          <InfoTable tableName='location' tablecolumn1='city' tablecolumn2='address' tablecolumn3='doors'/>
+        </div>
         <div className="weekly-traffic-grid"></div>
       </div>
     </div>
--- AccessKey-v1/accesskey.v1/src/features/infoTable/InfoTable.tsx
+++ AccessKey-v1/accesskey.v1/src/features/infoTable/InfoTable.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { ReactComponent as FilterIcon} from "./assets/Vectorfilter_svg.svg"
-import { ReactComponent as AddIcon} from "./assets/add_squre_svg.svg"
-import { ReactComponent as DeleteIcon} from "./assets/delete_squre_svg.svg"
-import { ReactComponent as RightArrowIcon} from "./assets/right_arrow_svg.svg"
+import { ReactComponent as FilterIcon } from "./assets/Vectorfilter_svg.svg"
+import { ReactComponent as AddIcon } from "./assets/add_squre_svg.svg"
+import { ReactComponent as DeleteIcon } from "./assets/delete_squre_svg.svg"
+import { ReactComponent as RightArrowIcon } from "./assets/right_arrow_svg.svg"
 
 const locationArrayExample = [
     {
@@ -26,48 +26,51 @@ const locationArrayExample = [
 ]
 interface InfoTableProps {
     tableName: string,
-    tablecolumn1:string,
-    tablecolumn2:string,
-    tablecolumn3:string,
+    tablecolumn1: string,
+    tablecolumn2: string,
+    tablecolumn3: string,
     // tableContent: string
 }
 
 export const InfoTable: React.FC<InfoTableProps> = (props) => {
-  return (
-    <div className='table'>
-        <div className='table__header'>
-            <div className='table__header__left'>
-                <h2>{props.tableName}</h2>
-                <FilterIcon/>
-                <div>This will be a search bar</div>
+    return (
+        <div className='table'>
+            <div className='table__header'>
+                <div className='table__header__left'>
+                    <h2>{props.tableName}</h2>
+                    <FilterIcon />
+                    <div>This will be a search bar</div>
+                </div>
+                <div className='table__header__right'>
+                    <AddIcon />
+                    <DeleteIcon />
+                </div>
             </div>
-            <div className='table__header__right'>
-                <AddIcon/>
-                <DeleteIcon/>
-            </div>
-        </div>
-        <div className='table__table'>
+            <div className='table__table'>
                 <table>
                     <tr>
-                       <th>{props.tableName}</th> 
-                       <th>{props.tablecolumn1}</th>
-                       <th>{props.tablecolumn2}</th>
-                       <th>{props.tablecolumn3}</th>
+                        <th></th>
+                        <th>{props.tableName}</th>
+                        <th>{props.tablecolumn1}</th>
+                        <th>{props.tablecolumn2}</th>
+                        <th>{props.tablecolumn3}</th>
+                        <th></th>
                     </tr>
                 </table>
                 {locationArrayExample.map((location) => {
-                    return(
+                    return (
                         <tr>
                             <input type="checkbox" />
                             <td>{location.locationName}</td>
                             <td>{location.city}</td>
                             <td>{location.address}</td>
                             <td>{location.doors}</td>
-                            <td>{<RightArrowIcon/>}</td>
+                            <td>{<RightArrowIcon />}</td>
                         </tr>
-                    ) })}
+                    )
+                })}
 
             </div>
-    </div>
-  )
+        </div>
+    )
 }
